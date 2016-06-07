4f320442390ca00f1a3d04165b98651a7fd26c73
Parvez Shaikh <parvez.shaikh@bookmyshow.com>
2016-06-07 14:44:57 +0530

    Interactive form completed

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $('.awesome-form .input-group input').focusout(function() {
+        var text_val = $(this).val();
+
+        if (text_val == "") {
+            $(this).removeClass('has-value');
+        } else {
+            $(this).addClass('has-value');
+        }
+    });
+
+    $("#email").on("focus", function(){
+    	if($(this).val() === "") {
+    		$("img").show();
+    	} else {
+    		$("img").hide();
+    	}
+    })
+
+    $("#email").on("keyup", function(){
+    	if($(this).val().length > 6) {
+    		$("img").show().attr("src","assets/images/1f613.png");
+    	} else {
+    		$("img").show();
+    	}
+
+    	if($(this).val().length === 23) {
+    		$("img").show().attr("src","assets/images/263a.png");
+    	} else {
+    		$("img").show();
+    	}
+    })
+
+    $("input[type='submit']").on("click", function(){
+    	if($("#name").val() === "") {
+    		alert("EMPTY NAME");
+    		return false;
+    	}
+    	if($("#email").val() === "") {
+    		alert("EMPTY EMAIL");
+    		return false;
+    	}
+    });
+});
